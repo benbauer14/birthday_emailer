0941c2e2d9a98480ad852f79cb4d00bd8d238c64
Ben Bauer <ben.bauer1@gmail.com>
2021-07-24 10:50:13 -0500

    sends email

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import smtplib
+#have to set security to lower settings in gmail to allow gmail to send
+my_email = ""
+my_password = ""
+
+connection = smtplib.SMTP("smtp.gmail.com")
+#encrypts email
+connection.starttls()
+connection.login(user=my_email, password=my_password)
+connection.sendmail(from_addr=my_email, to_addrs="bob@bob.com", msg="Subject:Hello\n\nThis is the body of the email.")
+connection.close()
+
